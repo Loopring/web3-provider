995dac229968e3431d5789e8b243ed9640f60ce7
windatang <windatang@gmail.com>
2022-03-23 00:43:41 +0800

    init lib

---
+++ src/command.ts
@@ -0,0 +1,56 @@
+import Web3 from "web3";
+import { walletServices } from "./walletServices";
+
+export enum Commands {
+  ConnectWallet = "ConnectWallet",
+  DisConnect = "DisConnect",
+  ChangeNetwork = "ChangeNetwork",
+  Processing = "Processing",
+  Error = "Error",
+}
+export enum ErrorType {
+  FailedConnect = "FailedConnect",
+}
+
+export enum ProcessingType {
+  waiting = "waiting",
+  nextStep = "nextStep",
+}
+
+export const ExtensionSubscribe = (provider: any, web3: Web3) => {
+  if (provider) {
+    provider.on("accountsChanged", (accounts: Array<string>) => {
+      if (accounts.length) {
+        walletServices.sendConnect(web3, provider);
+      } else {
+        walletServices.sendDisconnect(-1, "disconnect for no account");
+      }
+    });
+    provider.on("chainChanged", (chainId: number) => {
+      walletServices.sendConnect(web3, provider);
+    });
+    provider.on("disconnect", (code: number, reason: string) => {
+      walletServices.sendDisconnect(code, reason);
+    });
+  }
+};
+
+export const ExtensionUnsubscribe = async (provider: any) => {
+  if (provider && typeof provider.removeAllListeners === "function") {
+    // provider.removeAllListeners('accountsChanged');
+    // provider.removeAllListeners('chainChanged');
+    // provider.removeAllListeners('disconnect');
+    await provider.removeAllListeners();
+  }
+};
+
+export enum ConnectProviders {
+  unknown = "unknown",
+  MetaMask = "MetaMask",
+  WalletConnect = "WalletConnect",
+  Coinbase = "Coinbase",
+}
+export const RPC_URLS: { [chainId: number]: string } = {
+  1: process.env.REACT_APP_RPC_URL_1 as string,
+  5: process.env.REACT_APP_RPC_URL_5 as string,
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./command";
+export * from "./providers";
+export * from "./walletServices";
